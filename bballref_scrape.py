585422a05f838a89aba9fd6ffe0adeb25b107dbf
Dipika Sikka <dipikasikka1@gmail.com>
2019-04-13 21:00:52 -0400

    Script to scraped data, updated output file, and scraped data

---
+++ bballref_scrape.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from basketball_reference_web_scraper import client
+from basketball_reference_web_scraper import output
+
+# HAVE TO UPDATE YEAR VALUES AND CSV FILE NAMES FOR EACH SEASON
+
+year_month = {'first_half': {'year': 2017, 'months': np.linspace(10, 12, num=3)},
+'second_half': {'year': 2018, 'months': np.linspace(1, 6, num=6)}}
+days = np.linspace(1, 31, num=31)
+
+box_scores = {}
+
+# k gives first_half, second_half
+# v gives all the stored values
+for k,v in year_month.items():
+    for month in v['months']:
+        for day in days:
+            date_list = [str(v['year']), str(month), str(day)]
+            s = '-'
+            date_formatted =s.join(date_list)
+            box_score = client.team_box_scores(day=int(day), month=int(month),year=int(v['year']))
+            if len(box_score) is not 0:
+                box_scores.update({date_formatted: box_score})
+                
+output.team_box_scores_to_csv(box_scores, '2017to2018.csv', 'w')
+
