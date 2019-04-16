acec2f8957b17e9bb4ed93c7aaee0701c52609e3
seanlazaro <seanchristienlazaro@gmail.com>
2019-04-16 01:00:19 -0400

    Added script to get game averages and added season column to combined data

--- get_averages.py
+++ get_averages.py
@@ -9,42 +9,69 @@ Created on Sat Apr 13 17:10:06 2019
 import csv
 import time
 from basketball_reference_web_scraper import client
+import pandas as pd
+import pytz
 
 
+#script parameters
 season_end_year = 2018
 
 
 start_time = time.time() #for timing how this takes
 
-#averages_file = open('averages.csv', 'w')
-#
-#averages_file.write('header1,' + \
-#                    'header2' + \
-#                    '\n')
+df = pd.read_csv('seasons_combined.csv')
+df = df.sort_values(by = ['season', 'team', 'date'])
+
+averages_file = open('averages.csv', 'w')
+
+str_to_write = 'season,date,home_team'
+for i in range(2, len(df.columns) - 1):
+    str_to_write += ',' + df.columns[i] + '_home'
+for i in range(2, len(df.columns) - 1):
+    str_to_write += ',' + df.columns[i] + '_away'
+str_to_write += ',home_won\n'
+averages_file.write(str_to_write)
 
 
 games = client.season_schedule(season_end_year=season_end_year)
+season = str(season_end_year - 1) + '-' + str(season_end_year)
+est = pytz.timezone("US/Eastern")
 for game in games:
     
-    home_team_won = 0
+    #can't calculate average from last 6 games if 6 games haven't been played yet
+    date = game['start_time'].astimezone(est).strftime('%Y-%m-%d')
+    home_prev_games = df.loc[(df['season'] == season) & \
+                             (df['team'] == game['home_team'].value) & \
+                             (df['date'] < date)]
+    away_prev_games = df.loc[(df['season'] == season) & \
+                             (df['team'] == game['away_team'].value) & \
+                             (df['date'] < date)]
+    if home_prev_games.shape[0] < 6 or away_prev_games.shape[0] < 6:
+        continue
+    
+    
+    #get averages from last 6 games and winner
+    home_averages = home_prev_games.iloc[-6:].mean()
+    away_averages = away_prev_games.iloc[-6:].mean()
+    
+    home_won = 0
     if game['home_team_score'] > game['away_team_score']:
-        home_team_won = 1
+        home_won = 1
     
-    print(game['start_time'].strftime('%m/%d/%Y') + ': ' + game['home_team'].value + ' vs. ' + game['away_team'].value)
 
-
-#    data = [1, 2] #placeholder code
-#
-#    averages_str = str(data[0])
-#    averages_str += ',' + str(data[1])
-#    averages_str += '\n'
-#    averages_file.write(averages_str)
+    str_to_write = season + ',' + date + ',' + game['home_team'].value
+    for i in range(len(home_averages)):
+        str_to_write += ',' + str(home_averages[i])
+    for i in range(len(away_averages)):
+        str_to_write += ',' + str(away_averages[i])
+    str_to_write += ',' + str(home_won) + '\n'
+    averages_file.write(str_to_write)
     
     
-    break #only look at one game when testing code
+    print(date + ': ' + game['away_team'].value + ' at ' + game['home_team'].value)
     
     
-#averages_file.close()
+averages_file.close()
 
 end_time = time.time() #for timing how this takes
 print('Time required = %d s.' % round(end_time - start_time))
