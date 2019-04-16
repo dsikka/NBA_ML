8d5a8b94b2fe3be1c9edfc9a864fd6e4930549c7
Dipika Sikka <dipikasikka1@gmail.com>
2019-04-16 17:43:41 -0400

    Add seasons column to all combined data and combine seasons

--- bballref_scrape.py
+++ bballref_scrape.py
@@ -7,8 +7,8 @@ from basketball_reference_web_scraper import output
 
 # HAVE TO UPDATE YEAR VALUES AND CSV FILE NAMES FOR EACH SEASON
 
-year_month = {'first_half': {'year': 2017, 'months': np.linspace(10, 12, num=3)},
-'second_half': {'year': 2018, 'months': np.linspace(1, 6, num=6)}}
+year_month = {'first_half': {'year': 2012, 'months': np.linspace(10, 12, num=3)},
+'second_half': {'year': 2013, 'months': np.linspace(1, 6, num=6)}}
 days = np.linspace(1, 31, num=31)
 
 boxscores_1 = {}
@@ -29,10 +29,10 @@ for k,v in year_month.items():
                 boxscores_1.update({date_formatted: box_score_1})
                 boxscores_2.update({date_formatted: box_score_2})
 
-output.team_box_scores_to_csv_adv(boxscores_2, '2017to2018basic.csv', 'w')
-output.team_box_scores_to_csv(boxscores_1, '2017to2018adv.csv', 'w')
+output.team_box_scores_to_csv_adv(boxscores_2, '2012to2013adv.csv', 'w')
+output.team_box_scores_to_csv(boxscores_1, '2012to2013basic.csv', 'w')
 
-df1 = pd.read_csv('2017to2018basic.csv')
-df2 = pd.read_csv('2017to2018adv.csv')
+df1 = pd.read_csv('2012to2013adv.csv')
+df2 = pd.read_csv('2012to2013basic.csv')
 result = pd.concat([df2, df1], axis=1)
-result.to_csv("2017to2018combined.csv", index=False, encoding='utf-8-sig')
+result.to_csv("2012to2013combined.csv", index=False, encoding='utf-8-sig')
--- combine_seasons.py
+++ combine_seasons.py
@@ -1,26 +1,23 @@
 import os
 import pandas as pd 
+import re
 
-#files = ['2013to2014combined.csv', '2014to2015combined.csv', '2015to2016combined.csv', 
-#'2016to2017combined.csv', '2017to2018combined.csv']
-#
-#combined_csv = pd.concat([pd.read_csv(f) for f in files], axis=0)
-#combined_csv.to_csv("season_combined.csv", index=False, encoding='utf-8-sig')
+from os import path
 
-df2014 = pd.read_csv('./scraped_data_combined/2013to2014combined.csv')
-df2014['season'] = '2013-2014'
+data_path = 'scraped_data_combined'
+files = os.listdir(data_path)
 
-df2015 = pd.read_csv('./scraped_data_combined/2014to2015combined.csv')
-df2015['season'] = '2014-2015'
+# Add season column to each combined data file
 
-df2016 = pd.read_csv('./scraped_data_combined/2015to2016combined.csv')
-df2016['season'] = '2015-2016'
+for file in files:
+	fullPath = os.path.join(data_path, file)
+	df = pd.read_csv(fullPath)
+	s = re.match("(\w+)combined.csv", file).group(1)
+	s = s.replace("to", '-')
+	df.insert(0, 'Season', s)
+	df.to_csv(fullPath, index=False, encoding='utf-8-sig')
 
-df2017 = pd.read_csv('./scraped_data_combined/2016to2017combined.csv')
-df2017['season'] = '2016-2017'
 
-df2018 = pd.read_csv('./scraped_data_combined/2017to2018combined.csv')
-df2018['season'] = '2017-2018'
-
-combined_csv = pd.concat([df2014, df2015, df2016, df2017, df2018], axis=0)
-combined_csv.to_csv('seasons_combined.csv', index=False, encoding='utf-8-sig')
+# Combine all the seasons together
+combined_csv = pd.concat([pd.read_csv(os.path.join(data_path, file)) for file in files], axis=0)
+combined_csv.to_csv("season_combined.csv", index=False, encoding='utf-8-sig')
