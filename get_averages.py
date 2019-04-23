c075fc820f715efd70e2993ed2bc3941ab040bb7
seanlazaro <seanchristienlazaro@gmail.com>
2019-04-22 21:25:49 -0400

    Added during_playoffs column

--- get_averages.py
+++ get_averages.py
@@ -20,16 +20,17 @@ season_end_year = 2018
 start_time = time.time() #for timing how this takes
 
 df = pd.read_csv('season_combined.csv')
-df = df.sort_values(by = ['Season', 'team', 'date'])
+df = df.sort_values(by = ['season', 'team', 'date'])
 
 averages_file = open('averages/averages' + str(season_end_year - 1) + 'to' + \
                      str(season_end_year) + '.csv', 'w')
 
 str_to_write = 'season,date,home_team'
-for i in range(3, len(df.columns)):
+for i in range(3, len(df.columns) - 1):
     str_to_write += ',' + df.columns[i] + '_home'
-for i in range(3, len(df.columns)):
+for i in range(3, len(df.columns) - 1):
     str_to_write += ',' + df.columns[i] + '_away'
+str_to_write += ',' + df.columns[-1]
 str_to_write += ',home_won\n'
 averages_file.write(str_to_write)
 
@@ -41,10 +42,10 @@ for game in games:
     
     #can't calculate average from last 6 games if 6 games haven't been played yet
     date = game['start_time'].astimezone(est).strftime('%Y-%m-%d')
-    home_prev_games = df.loc[(df['Season'] == season) & \
+    home_prev_games = df.loc[(df['season'] == season) & \
                              (df['team'] == game['home_team'].value) & \
                              (df['date'] < date)]
-    away_prev_games = df.loc[(df['Season'] == season) & \
+    away_prev_games = df.loc[(df['season'] == season) & \
                              (df['team'] == game['away_team'].value) & \
                              (df['date'] < date)]
     if home_prev_games.shape[0] < 6 or away_prev_games.shape[0] < 6:
@@ -55,16 +56,20 @@ for game in games:
     home_averages = home_prev_games.iloc[-6:].mean()
     away_averages = away_prev_games.iloc[-6:].mean()
     
+    curr_game = df.loc[(df['team'] == game['home_team'].value) & \
+                       (df['date'] == date)]
+    
     home_won = 0
     if game['home_team_score'] > game['away_team_score']:
         home_won = 1
     
 
     str_to_write = season + ',' + date + ',' + game['home_team'].value
-    for i in range(len(home_averages)):
+    for i in range(len(home_averages) - 1):
         str_to_write += ',' + str(home_averages[i])
-    for i in range(len(away_averages)):
+    for i in range(len(away_averages) - 1):
         str_to_write += ',' + str(away_averages[i])
+    str_to_write += ',' + str(curr_game['during_playoffs'].values[0])
     str_to_write += ',' + str(home_won) + '\n'
     averages_file.write(str_to_write)
     
